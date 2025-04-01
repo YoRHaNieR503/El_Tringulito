4a74312f4b0fb59615d9946c56426626ddb6fc6a
Kevin Quiñonez <kquinonez@gmail.com>
2025-04-01 14:52:33 -0600

    Combos

---
+++ El_Tringulito/Models/combos.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace El_Tringulito.Models
+{
+    [Table("combos")]
+    public class combos
+    {
+        [Key]
+        public int id_combo { get; set; }
+
+        public string? nombre { get; set; }
+
+        public decimal precio { get; set; }
+    }
+}

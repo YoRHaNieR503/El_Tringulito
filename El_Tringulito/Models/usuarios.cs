0d85d9b1d587823eff01fdaec44860c0807c3452
quxly4 <quely004@gmail.com>
2025-04-01 15:27:39 -0600

    usuarios

---
+++ El_Tringulito/Models/usuarios.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace El_Tringulito.Models
+{
+    [Table("usuarios")]
+    public class Usuario
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Column("usuario")] 
+    public string NombreUsuario { get; set; }
+
+    [Required]
+    [StringLength(255)]
+    [Column("contrasenia")] 
+        public string Contrasenia { get; set; }
+    }
+}
+

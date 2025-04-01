8d92688ec747c37c39871c57e542370f4c44f229
Kevin Quiñonez <kquinonez@gmail.com>
2025-04-01 14:44:21 -0600

    Platos

---
+++ El_Tringulito/Models/platos.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace El_Tringulito
+    .Models
+{
+    [Table("platos")]
+    public class Platos
+    {
+        [Key]
+        public int id_plato { get; set; }
+
+        public string nombre { get; set; }
+
+        public decimal precio { get; set; }
+    }
+}

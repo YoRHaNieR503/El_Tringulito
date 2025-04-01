27874bac68c9fa16c6fa029ab3fe2635d219aa1a
quxly4 <quely004@gmail.com>
2025-04-01 14:47:18 -0600

    mesas

---
+++ El_Tringulito/Models/mesas.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ElTriangulitoP.Models
+{
+    [Table("mesas")]
+    public class Mesas
+    {
+        [Key]
+        public int id_mesa { get; set; }
+
+        public string? nombre { get; set; }
+
+        public string? estado { get; set; }
+    }
+}

62b76d0a6b912c84298e720a32ac3de8e05ec1ee
Chris <christopher.alarcon@catolica.edu.sv>
2025-04-01 13:39:49 -0600

    context

---
+++ El_Tringulito/Models/ElTriangulitoDBContext.cs
@@ -0,0 +1,20 @@
+﻿using El_Tringulito.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace El_Tringulito.Models
+{
+    public class ElTriangulitoDBContext : DbContext
+    {
+        public ElTriangulitoDBContext(DbContextOptions<ElTriangulitoDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<combos> combos { get; set; }
+        public DbSet<Mesas> mesas { get; set; }
+        public DbSet<Promociones> promociones { get; set; }
+        public DbSet<Platos> platos { get; set; }
+        public DbSet<Ordenes> ordenes { get; set; }
+
+        
+    }
+}
--- El_Tringulito/Program.cs
+++ El_Tringulito/Program.cs
@@ -1,7 +1,16 @@
+using El_Tringulito.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ElTriangulitoDBContext>(options =>
+        options.UseSqlServer(
+                builder.Configuration.GetConnectionString("elTriangulitoDbConnection")
+            )
+        );
 
 var app = builder.Build();
 

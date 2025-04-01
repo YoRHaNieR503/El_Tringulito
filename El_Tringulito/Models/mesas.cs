9005e45d31a8006393194d2cd4983f28e41fdaa1
quxly4 <quely004@gmail.com>
2025-04-01 14:49:22 -0600

    .

--- El_Tringulito/Models/mesas.cs
+++ El_Tringulito/Models/mesas.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace ElTriangulitoP.Models
+namespace El_Tringulito.Models
 {
     [Table("mesas")]
     public class Mesas
